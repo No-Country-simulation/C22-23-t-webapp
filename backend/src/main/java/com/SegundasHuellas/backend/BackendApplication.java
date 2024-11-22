6b05fce988625e69061bf8cf6a8cdf91d6ffc81e
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-22 18:44:36 -0300

    remove exception 🤦‍♂️

--- backend/src/main/java/com/SegundasHuellas/backend/BackendApplication.java
+++ backend/src/main/java/com/SegundasHuellas/backend/BackendApplication.java
@@ -1,18 +1,13 @@
 package com.SegundasHuellas.backend;
 
-import com.SegundasHuellas.backend.shared.error.DomainException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static com.SegundasHuellas.backend.shared.error.DomainException.ErrorCode.RESOURCE_NOT_FOUND;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-
-		throw new DomainException(RESOURCE_NOT_FOUND);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
