cdd66f0a77545c22c19aee5a8cf352d353685c09
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-22 18:25:16 -0300

    create entity

---
+++ backend/src/main/java/com/SegundasHuellas/backend/pets/domain/Pet.java
@@ -0,0 +1,46 @@
+package com.SegundasHuellas.backend.pets.domain;
+
+import com.SegundasHuellas.backend.shared.audit.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "pets")
+public class Pet extends BaseEntity {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    //Breed-Entity -> Species-Enum
+
+    private Integer age;
+
+    private Boolean isVaccinated;
+
+    private Boolean isSterilized;
+
+    private String healthStatus;
+
+    //private Image photo;
+
+    private LocalDate birthDate;
+
+    private Gender gender;
+
+    private Double weight;
+
+    public enum Gender {
+        MALE, FEMALE
+    }
+
+
+}
