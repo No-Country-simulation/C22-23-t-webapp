6ac71257b8e743ed1408ed684173d68abccbff65
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-11-23 12:25:40 -0300

    Add index.js for simplifying component imports

---
+++ frontend/src/components/index.js
@@ -0,0 +1,8 @@
+// La idea de este archivo es que solamente hayan exports como el siguiente comentario:
+//  export * from "./LandingPageContainer/LandingPageContainer.jsx"
+//  export * from "./NavBar/NavBar.jsx"
+
+// Esto simplifica los imports de los componentes, quedando algo como:
+//  import { LandingPageContainer, NavBar } from './components'
+
+// Fuente para mas contexto: https://youtu.be/xt6Vi1Jd1TE&t=1255
