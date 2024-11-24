d87764f4ae01b9e59cf85c58f4947e5251e30e2a
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-24 11:04:20 -0300

    update entity to only hold the original url

--- backend/src/main/java/com/SegundasHuellas/backend/shared/domain/vo/Image.java
+++ backend/src/main/java/com/SegundasHuellas/backend/shared/domain/vo/Image.java
@@ -15,32 +15,32 @@ public class Image {
     @Column(name = "original_url")
     private String originalUrl;
 
-    @Column(name = "thumbnail_url")
-    private String thumbnailUrl;
-
 
     protected Image() {
     }
 
     private Image(String originalUrl) {
         this.originalUrl = Objects.requireNonNull(originalUrl, "Original URL cannot be null");
-        this.thumbnailUrl = adjustForLowSize(originalUrl);
     }
 
     public static Image fromUrl(String url) {
         return new Image(url);
     }
 
+    public String getThumbnailUrl() {
+        return adjustForLowSize(originalUrl);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Image image)) return false;
-        return Objects.equals(originalUrl, image.originalUrl) && Objects.equals(thumbnailUrl, image.thumbnailUrl);
+        return Objects.equals(originalUrl, image.originalUrl);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(originalUrl, thumbnailUrl);
+        return Objects.hashCode(originalUrl);
     }
 
     private static String adjustForLowSize(String secureUrl) {
