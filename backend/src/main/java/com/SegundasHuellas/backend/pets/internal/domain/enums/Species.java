00c616425cf44903f801fe67e23a998c63b63ea1
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-25 15:33:43 -0300

    add the default breed name for each species

--- backend/src/main/java/com/SegundasHuellas/backend/pets/internal/domain/enums/Species.java
+++ backend/src/main/java/com/SegundasHuellas/backend/pets/internal/domain/enums/Species.java
@@ -1,5 +1,13 @@
 package com.SegundasHuellas.backend.pets.internal.domain.enums;
 
 public enum Species {
-    DOG, CAT, OTHER
+    DOG,
+    CAT,
+    OTHER;
+
+
+    public String getDefaultBreedName() {
+        // También se podría dar un nombre distinto por especie, como "Raza de Gato Desconocida", pero quiza no es necesario.
+        return "Raza Desconocida";
+    }
 }
