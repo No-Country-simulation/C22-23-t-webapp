a7cce3dd4c48c17965bce0df15a519929bfc1ba4
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-11-25 04:31:46 -0300

    Add primitive version of LandingPageHero component with its css styling

---
+++ frontend/src/components/LandingPageHero/LandingPageHero.jsx
@@ -0,0 +1,19 @@
+import './LandingPageHero.css'
+
+export function LandingPageHero() {
+    return (
+        <div id="LandingPageHero">
+            <div id="LandingPageHeroContainer">
+
+                <h2 id="LandingPageHeroTitle">Encuentra a tu compañero perfecto</h2>
+
+                <p id="LandingPageHeroDescription">
+                Dale un hogar a una mascota y cambia su vida y la tuya para siempre.
+                </p>
+
+                <button id="LandingPageHeroButton">Empieza a adoptar</button>
+
+            </div>
+        </div>
+    )
+}
