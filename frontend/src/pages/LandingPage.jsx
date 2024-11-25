b88328247e87c3c9dd95c87f8b7f0db8f9484038
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-11-25 04:34:53 -0300

    Add LandingPage.jsx into pages directory for modular implementation of component imports

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import { LandingPageHeader, LandingPageHero, LandingPageFeaturesContainer, LandingPageFooter } from '../components/'
+
+export function LandingPage() {
+    return (
+        <>
+            <LandingPageHeader />
+            <LandingPageHero />
+            <LandingPageFeaturesContainer />
+            <LandingPageFooter />
+        </>
+    )
+}
