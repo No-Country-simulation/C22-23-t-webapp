dc5dfd9129c437827e9f0b0174e7b1b84678d8e3
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-25 15:07:14 -0300

    remove annotation / not needed on interfaces|jpa repos

--- backend/src/main/java/com/SegundasHuellas/backend/pets/internal/infra/persistence/BreedRepository.java
+++ backend/src/main/java/com/SegundasHuellas/backend/pets/internal/infra/persistence/BreedRepository.java
@@ -3,11 +3,9 @@ package com.SegundasHuellas.backend.pets.internal.infra.persistence;
 import com.SegundasHuellas.backend.pets.internal.domain.entity.Breed;
 import com.SegundasHuellas.backend.pets.internal.domain.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface BreedRepository extends JpaRepository<Breed, Long> {
 
     Optional<Breed> findByNameAndSpecies(String name, Pet.Species species);
