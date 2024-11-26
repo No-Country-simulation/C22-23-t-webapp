dc40a12542577385479638d74b07e4c55e643a11
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-26 13:58:59 -0300

    fix configurations to properly work with custom factories

--- backend/src/main/java/com/SegundasHuellas/backend/shared/infrastructure/storage/config/UploadConfig.java
+++ backend/src/main/java/com/SegundasHuellas/backend/shared/infrastructure/storage/config/UploadConfig.java
@@ -8,22 +8,19 @@ import java.util.Map;
 @Value
 @Builder
 public class UploadConfig {
-    String identifier;
-    String folder;
+
     Map<String, Object> options;
 
-    public static UploadConfig of(String folder, String identifier) {
-        return UploadConfig.builder()
-                           .folder(folder)
-                           .identifier(identifier)
-                           .options(Map.of())
-                           .build();
-    }
+//    public static UploadConfig of(String folder, String identifier) {
+//        return UploadConfig.builder()
+//                           .folder(folder)
+//                           .identifier(identifier)
+//                           .options(Map.of())
+//                           .build();
+//    }
 
-    public static UploadConfig of(String folder, String identifier, Map<String, Object> options) {
+    public static UploadConfig of(Map<String, Object> options) {
         return UploadConfig.builder()
-                           .folder(folder)
-                           .identifier(identifier)
                            .options(options)
                            .build();
     }
