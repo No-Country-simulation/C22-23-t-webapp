ea3ed727cda5b782562daa70fd68a6e6120963c4
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-26 11:36:01 -0300

    add translation to enum for ease of use

--- backend/src/main/java/com/SegundasHuellas/backend/pets/internal/domain/enums/Species.java
+++ backend/src/main/java/com/SegundasHuellas/backend/pets/internal/domain/enums/Species.java
@@ -1,12 +1,22 @@
 package com.SegundasHuellas.backend.pets.internal.domain.enums;
 
+import lombok.Getter;
+
+
+@Getter
 public enum Species {
-    DOG,
-    CAT,
-    OTHER;
+    DOG("Perro"),
+    CAT("Gato"),
+    OTHER("Otro");
 
 
-    public static final String DEFAULT_BREED_PLACEHOLDER = "Raza sin definir";
+    public static final String DEFAULT_BREED_PLACEHOLDER = "Sin raza especificada";
+
+    private final String translation;
+
+    Species(String translation) {
+        this.translation = translation;
+    }
 
     public String getDefaultBreedName() {
         // También se podría dar un nombre distinto por especie, como "Raza de Gato Desconocida", pero quiza no es necesario.
