8e56383eee41df80b7f9aa7a82c480088f3b2dbd
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-26 11:31:15 -0300

    add translation to gender enum for ease of use

--- backend/src/main/java/com/SegundasHuellas/backend/pets/internal/domain/enums/Gender.java
+++ backend/src/main/java/com/SegundasHuellas/backend/pets/internal/domain/enums/Gender.java
@@ -1,5 +1,17 @@
 package com.SegundasHuellas.backend.pets.internal.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Gender {
-    MALE, FEMALE, UNDEFINED
+    MALE("Masculino"),
+    FEMALE("Femenino"),
+    UNDEFINED("No definido");
+
+    private final String translation;
+
+    Gender(String translation) {
+        this.translation = translation;
+    }
+
 }
