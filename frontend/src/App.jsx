a3ba5f97dcb098ccc0b9c22175765a2d11d487a9
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-11-27 14:47:40 -0300

    Add WIP_MESSAGE and ERROR_MESSAGE for future paths of the Frontend project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { LandingPage } from './pages/LandingPage.jsx'
 
 function App() {
+    const WIP_MESSAGE = "Página aún en construcción..."
+    const ERROR_MESSAGE = "¡UPS! Esa página no existe..."
+
     return (
         <BrowserRouter basename="/SegundasHuellas/">
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="/home" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
             </Routes>
         </BrowserRouter>
     )
