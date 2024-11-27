bad8536700283610c2ede219719566924ac0339d
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-11-27 14:37:01 -0300

    Add BrowserRouter alongside basename config to Frontend project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { LandingPage } from './pages/LandingPage.jsx'
 
 function App() {
-  return (
-    <>
-      <LandingPage />
-    </>
-  )
+    return (
+        <BrowserRouter basename="/SegundasHuellas/">
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
