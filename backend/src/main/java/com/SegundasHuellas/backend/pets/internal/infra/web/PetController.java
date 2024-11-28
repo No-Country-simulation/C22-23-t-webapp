46ca11ac08cf69401ff2d5f228bdb52d107731b0
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-27 22:41:26 -0300

    added pageable defaults

--- backend/src/main/java/com/SegundasHuellas/backend/pets/internal/infra/web/PetController.java
+++ backend/src/main/java/com/SegundasHuellas/backend/pets/internal/infra/web/PetController.java
@@ -3,14 +3,13 @@ package com.SegundasHuellas.backend.pets.internal.infra.web;
 import com.SegundasHuellas.backend.pets.api.dto.*;
 import com.SegundasHuellas.backend.pets.internal.application.service.PetSearchService;
 import com.SegundasHuellas.backend.pets.internal.application.service.PetService;
+import com.SegundasHuellas.backend.pets.internal.domain.enums.PetStatus;
+import com.SegundasHuellas.backend.pets.internal.domain.enums.Species;
 import com.SegundasHuellas.backend.shared.application.dto.PageResponse;
 import jakarta.validation.Valid;
-import lombok.NoArgsConstructor;
+import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +25,6 @@ import static org.springframework.data.domain.Sort.Direction.ASC;
 public class PetController {
 
     private final PetService petService;
-
     private final PetSearchService petSearchService;
 
     @PostMapping
@@ -49,9 +47,13 @@ public class PetController {
 
     @GetMapping("/search")
     public ResponseEntity<PageResponse<PetSearchResult>> searchPets(
-            @ModelAttribute PetSearchCriteria criteria,
+            @RequestParam(required = false, value = "name") String name,
+            @RequestParam(required = false, value = "species") Species species,
+            @RequestParam(required = false, value = "breed") String breed,
+            @RequestParam(required = false, value = "status") PetStatus status,
             @PageableDefault(sort = "id", direction = ASC) Pageable pageable
     ) {
+        PetSearchCriteria criteria = new PetSearchCriteria(name, species, breed, status);
         return ResponseEntity.ok(petSearchService.searchPets(criteria, pageable));
     }
 
@@ -59,7 +61,7 @@ public class PetController {
     public ResponseEntity<PetResponseDto> updatePet(
             @PathVariable Long petId,
             @RequestBody UpdatePetRequestDto petDto
-            ) {
+    ) {
         PetResponseDto petResponseDto = petService.updatePet(petId, petDto);
         return ResponseEntity.ok(petResponseDto);
     }
