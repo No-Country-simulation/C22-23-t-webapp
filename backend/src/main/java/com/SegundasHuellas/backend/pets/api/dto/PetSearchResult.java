e510c206419bbabbb669d3f14c0b9f5b59b9f420
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-27 21:41:11 -0300

    Refactor PetSearchResult to simplify data structure and add JPQL constructor for mapping. Changed species, gender, and status fields to String for better compatibility, and included a constructor to facilitate JPQL queries.

--- backend/src/main/java/com/SegundasHuellas/backend/pets/api/dto/PetSearchResult.java
+++ backend/src/main/java/com/SegundasHuellas/backend/pets/api/dto/PetSearchResult.java
@@ -4,13 +4,22 @@ import com.SegundasHuellas.backend.pets.internal.domain.enums.Gender;
 import com.SegundasHuellas.backend.pets.internal.domain.enums.PetStatus;
 import com.SegundasHuellas.backend.pets.internal.domain.enums.Species;
 import com.SegundasHuellas.backend.shared.application.dto.ImageResponse;
+import com.SegundasHuellas.backend.shared.domain.vo.Image;
 
 public record PetSearchResult(
         Long id,
         String name,
-        Species species,
+        String species,
         Integer ageInDays,
-        Gender gender,
-        PetStatus status,
+        String gender,
+        String status,
         ImageResponse photo
-) {}
+) {
+
+    //Constructor for JPQL
+    public PetSearchResult(Long id, String name, Species species, Integer ageInDays, Gender gender, PetStatus status, Image photo) {
+        this(id, name, species.getTranslation(), ageInDays, gender.getTranslation(), status.getTranslation(), ImageResponse.from(photo));
+    }
+
+
+}
