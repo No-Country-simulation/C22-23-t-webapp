9765bf93b997f6fe4f6b8158eb55df8930f8ab35
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-29 08:36:25 -0300

    Remove commented-out methods from PetRepository

--- backend/src/main/java/com/SegundasHuellas/backend/pets/internal/infra/persistence/PetRepository.java
+++ backend/src/main/java/com/SegundasHuellas/backend/pets/internal/infra/persistence/PetRepository.java
@@ -10,11 +10,4 @@ import java.util.List;
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
-//    List<Pet> findBySpecies(Species species);
-
-    List<Pet> findByStatus(PetStatus status);
-
-    List<Pet> findByBreedName(String breedName);
-
-//    List<Pet> findBySpeciesAndStatus(Species species, PetStatus status);
 }
