77ff388ff2386fcddf176d0aef5a38950e6346bd
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-29 08:37:35 -0300

    Add PetReferenceDataController to handle reference data for pets like species,breeds and gender

--- backend/src/main/java/com/SegundasHuellas/backend/pets/internal/infra/web/PetReferenceDataController.java
+++ backend/src/main/java/com/SegundasHuellas/backend/pets/internal/infra/web/PetReferenceDataController.java
@@ -1,7 +1,10 @@
 package com.SegundasHuellas.backend.pets.internal.infra.web;
 
 import com.SegundasHuellas.backend.pets.internal.application.dto.BreedResponse;
+import com.SegundasHuellas.backend.pets.internal.application.dto.GenderResponse;
+import com.SegundasHuellas.backend.pets.internal.application.dto.SpeciesResponse;
 import com.SegundasHuellas.backend.pets.internal.application.service.BreedService;
+import com.SegundasHuellas.backend.pets.internal.domain.enums.Gender;
 import com.SegundasHuellas.backend.pets.internal.domain.enums.Species;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,21 +12,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/breeds")
+@RequestMapping("/api/pets/reference-data")
 @RequiredArgsConstructor
-public class BreedsController {
+public class PetReferenceDataController {
 
-    /*
-     * Si este controlador o su lógica asociada crece mucho, separar en su propio módulo
-     * */
     private final BreedService breedService;
 
-    @GetMapping
+    @GetMapping("/breeds")
     public List<BreedResponse> getAllAvailableBreedsForSpecies(@RequestParam("species") Species species) {
         return breedService.getAllBreedsForSpecies(species);
+    }
+
+    @GetMapping("/species")
+    public List<SpeciesResponse> getAllSpecies() {
+        return Arrays.stream(Species.values())
+                     .map(SpeciesResponse::new).toList();
+
+    }
+
+    @GetMapping("/genders")
+    public List<GenderResponse> getAllGenders() {
+        return Arrays.stream(Gender.values())
+                     .map(GenderResponse::new).toList();
     }
 
 
