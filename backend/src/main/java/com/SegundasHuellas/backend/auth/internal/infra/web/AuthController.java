13ca3ec468c245dd5e9441b8b04b0aaf7fe15abd
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-11-30 18:44:10 -0300

    Add AuthController to handle authentication endpoints

---
+++ backend/src/main/java/com/SegundasHuellas/backend/auth/internal/infra/web/AuthController.java
@@ -0,0 +1,35 @@
+package com.SegundasHuellas.backend.auth.internal.infra.web;
+
+import com.SegundasHuellas.backend.auth.internal.application.dto.LoginRequest;
+import com.SegundasHuellas.backend.auth.internal.application.dto.RegistrationRequest;
+import com.SegundasHuellas.backend.auth.internal.application.dto.TokenResponse;
+import com.SegundasHuellas.backend.auth.internal.application.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public TokenResponse register(@RequestBody @Valid RegistrationRequest registrationRequest) {
+        return authService.register(registrationRequest);
+    }
+
+    @PostMapping("/login")
+    public TokenResponse login(@RequestBody @Valid LoginRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
+
+    @PostMapping("/refresh")
+    public TokenResponse refreshToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+        return authService.refreshToken(authHeader);
+    }
+
+}
