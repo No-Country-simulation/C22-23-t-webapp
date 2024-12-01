a822629b01a8fc4c5bcfa4a364684bda83b43869
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-12-01 00:26:42 -0300

    Enhance TokenResponse DTO with additional fields and validation. Added 'expiresAt' and 'tokenType' fields, both marked as @NotNull, to improve token management. Introduced a static method 'bearer' for convenient creation of bearer token responses.

--- backend/src/main/java/com/SegundasHuellas/backend/auth/internal/application/dto/TokenResponse.java
+++ backend/src/main/java/com/SegundasHuellas/backend/auth/internal/application/dto/TokenResponse.java
@@ -1,7 +1,24 @@
 package com.SegundasHuellas.backend.auth.internal.application.dto;
 
+import com.SegundasHuellas.backend.auth.internal.domain.entity.Token;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.Instant;
+
 public record TokenResponse(
+        @NotNull
         String token,
-        String refreshToken
+        @NotNull
+        String refreshToken,
+
+        @NotNull
+        Instant expiresAt,
+
+        @NotNull
+        Token.TokenType tokenType
+
 ) {
+    public static TokenResponse bearer(String token, String refreshToken, Instant expiresAt) {
+        return new TokenResponse(token, refreshToken, expiresAt, Token.TokenType.BEARER);
+    }
 }
