c0b5011fef73127245dc9e8ea59f7c818ce98295
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-12-01 19:30:48 -0300

    Normalize code formatting in App.jsx <Route> elements
    It's just #OCD, nothing special about this :)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,17 +9,17 @@ function App() {
     return (
         <BrowserRouter basename="/SegundasHuellas/">
             <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/home" element={<SearchPage />} />
-                <Route path="/pet" element={<h2>{WIP_MESSAGE}</h2>} />
-                <Route path="/register" element={<h2>{WIP_MESSAGE}</h2>} />
-                <Route path="/login" element={<h2>{WIP_MESSAGE}</h2>} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/mission" element={<MissionPage />} />
-                <Route path="/contact" element={<ContactPage />} />
-                <Route path="/tips" element={<TipsPage />} />
-                <Route path="/process" element={<ProcessPage />} />
-                <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
+                <Route path="/"         element={ <LandingPage />            } />
+                <Route path="/home"     element={ <SearchPage />             } />
+                <Route path="/pet"      element={ <h2>{ WIP_MESSAGE }</h2>   } />
+                <Route path="/register" element={ <h2>{ WIP_MESSAGE }</h2>   } />
+                <Route path="/login"    element={ <h2>{ WIP_MESSAGE }</h2>   } />
+                <Route path="/about"    element={ <AboutPage />              } />
+                <Route path="/mission"  element={ <MissionPage />            } />
+                <Route path="/contact"  element={ <ContactPage />            } />
+                <Route path="/tips"     element={ <TipsPage />               } />
+                <Route path="/process"  element={ <ProcessPage />            } />
+                <Route path="*"         element={ <h2>{ ERROR_MESSAGE }</h2> } />
             </Routes>
         </BrowserRouter>
     )
