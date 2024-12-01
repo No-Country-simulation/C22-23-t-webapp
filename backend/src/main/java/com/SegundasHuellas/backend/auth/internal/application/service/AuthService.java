a235ffcf3dca5f4ac7e2a2902a363e12494cbdf9
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-12-01 00:27:09 -0300

    Refactor AuthService to enhance authentication flow and event handling. Updated registration and login methods to return AuthenticationResponse instead of TokenResponse, incorporating user event publishing for registration and login success. Improved error handling for locked accounts and invalid tokens, and streamlined token management with new helper methods. Added validation for user registration to prevent duplicate emails.

--- backend/src/main/java/com/SegundasHuellas/backend/auth/internal/application/service/AuthService.java
+++ backend/src/main/java/com/SegundasHuellas/backend/auth/internal/application/service/AuthService.java
@@ -1,104 +1,149 @@
 package com.SegundasHuellas.backend.auth.internal.application.service;
 
+import com.SegundasHuellas.backend.auth.api.events.UserAccountLockedEvent;
+import com.SegundasHuellas.backend.auth.api.events.UserLoggedInEvent;
+import com.SegundasHuellas.backend.auth.api.events.UserRegisteredEvent;
+import com.SegundasHuellas.backend.auth.internal.application.dto.AuthenticationResponse;
 import com.SegundasHuellas.backend.auth.internal.application.dto.LoginRequest;
 import com.SegundasHuellas.backend.auth.internal.application.dto.RegistrationRequest;
 import com.SegundasHuellas.backend.auth.internal.application.dto.TokenResponse;
 import com.SegundasHuellas.backend.auth.internal.domain.entity.Token;
 import com.SegundasHuellas.backend.auth.internal.domain.entity.User;
+import com.SegundasHuellas.backend.auth.internal.domain.enums.UserRole;
 import com.SegundasHuellas.backend.auth.internal.infra.persistence.TokenRepository;
 import com.SegundasHuellas.backend.auth.internal.infra.persistence.UserRepository;
+import com.SegundasHuellas.backend.shared.exception.DomainException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Set;
 
+import static com.SegundasHuellas.backend.auth.internal.application.exceptions.AuthErrorCode.*;
+import static com.SegundasHuellas.backend.shared.exception.DomainException.ErrorCode.DUPLICATED_DATA;
+
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
+    public static final String BEARER_PREFIX = "Bearer ";
+    public static final int BEARER_PREFIX_LENGTH = 7;
+    private final ApplicationEventPublisher eventPublisher;
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-    public TokenResponse register(RegistrationRequest request) {
 
+    public AuthenticationResponse register(RegistrationRequest request) {
+        validateRegistration(request);
 
         User user = User.builder()
                         .email(request.email())
-                        .password(passwordEncoder.encode(request.password())) //should do checks on the password confirmation
+                        .password(passwordEncoder.encode(request.password()))
+                        .roles(Set.of(UserRole.USER))
                         .build();
 
-        User savedUser = userRepository.save(user);
+        user = userRepository.save(user);
 
-        String jwtToken = jwtService.generateToken(user);
-        String refreshToken = jwtService.generateRefreshToken(user);
-        saveUserToken(savedUser, jwtToken);
-        return new TokenResponse(jwtToken, refreshToken);
+        TokenResponse tokens = generateTokens(user);
+        eventPublisher.publishEvent(new UserRegisteredEvent(user.getId(), user.getEmail()));
+
+        return AuthenticationResponse.from(user, tokens);
     }
 
-    public TokenResponse login(LoginRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.email(),
-                        request.password()
-
-                )
-        );
-
+    @Transactional(noRollbackFor = {DomainException.class})
+    public AuthenticationResponse login(LoginRequest request) {
         User user = userRepository.findByEmail(request.email())
-                                  .orElseThrow();//TODO: throw exception
+                                  .orElseThrow(() -> new DomainException(INVALID_CREDENTIALS));
+        if (user.isLocked()) {
+            throw new DomainException(ACCOUNT_LOCKED);
+        }
 
-        String jwtToken = jwtService.generateToken(user);
-        String refreshToken = jwtService.generateRefreshToken(user);
-        revokeAllUserTokens(user); //SOlo 1 cliente por logeado?
-        saveUserToken(user, jwtToken);
-        return new TokenResponse(jwtToken, refreshToken);
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.email(),
+                            request.password())
+            );
 
+            handleSuccessfulLogin(user);
+            TokenResponse tokens = generateTokens(user);
+            return AuthenticationResponse.from(user, tokens);
+
+
+        } catch (AuthenticationException e) {
+            handleFailedLogin(user);
+
+            log.warn("Login failed for user [{}]: {}", request.email(), e.getClass());
+            throw new DomainException(INVALID_CREDENTIALS);
+        }
     }
 
     public TokenResponse refreshToken(String authHeader) {
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            throw new IllegalArgumentException("Invalid Authorization token"); // TODO: USE DOMAIN EXCEPTION
+        if (authHeader == null || !authHeader.startsWith(BEARER_PREFIX)) {
+            throw new DomainException(INVALID_REFRESH_TOKEN);
         }
-        String refreshToken = authHeader.substring(7);
-        String userEmail  = jwtService.extractUsername(refreshToken);
+        String refreshToken = authHeader.substring(BEARER_PREFIX_LENGTH);
+        String userEmail = jwtService.extractUsername(refreshToken);
 
         if (userEmail == null) {
-            throw new IllegalArgumentException("Invalid Authorization token"); // TODO: USE DOMAIN EXCEPTION
+            throw new DomainException(INVALID_REFRESH_TOKEN);
         }
 
         User user = userRepository.findByEmail(userEmail)
-                                  .orElseThrow();//DOMAIN EXCEPTION or UsernameNotFoundException? tutorial says the second.
+                                  .orElseThrow(() -> new DomainException(INVALID_CREDENTIALS));
 
 
         if (!jwtService.isTokenValid(refreshToken, user)) {
-            throw new IllegalArgumentException("Invalid Authorization token"); // TODO: USE DOMAIN EXCEPTION??
+            throw new DomainException(INVALID_REFRESH_TOKEN);
         }
 
-        String accessToken = jwtService.generateToken(user);
         revokeAllUserTokens(user);
+        return generateTokens(user);
+    }
+
+    private void validateRegistration(RegistrationRequest request) {
+        if (userRepository.existsByEmail(request.email())) {
+            throw new DomainException(DUPLICATED_DATA, "Email already registered");
+        }
+    }
+
+    private void handleSuccessfulLogin(User user) {
+        user.recordSuccessfulLogin();
+        revokeAllUserTokens(user);
+        eventPublisher.publishEvent(new UserLoggedInEvent(user.getId(), user.getEmail()));
+    }
+
+    private void handleFailedLogin(User user) {
+        user.incrementFailedAttemptsCount();
+
+        if (user.isLocked()) {
+            eventPublisher.publishEvent(new UserAccountLockedEvent(user.getId(), user.getEmail()));
+        }
+    }
+
+
+    private TokenResponse generateTokens(User user) {
+        String accessToken = jwtService.generateToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
         saveUserToken(user, accessToken);
 
-        return new TokenResponse(accessToken, refreshToken);
-
+        return TokenResponse.bearer(accessToken, refreshToken, jwtService.extractExpiration(accessToken));
     }
 
     private void revokeAllUserTokens(User user) {
-        List<Token> validUserTokens = tokenRepository.findAllValidIsFalseOrRevokedIsFalseByUserId(user.getId());
-
-        if (!validUserTokens.isEmpty()) {
-            for (Token token : validUserTokens) {
-                token.setRevoked(true);
-                token.setExpired(true);
-            }
-            tokenRepository.saveAll(validUserTokens);
-        }
+        tokenRepository.revokeAllUserTokens(user.getId());
     }
 
     private void saveUserToken(User savedUser, String jwtToken) {
@@ -114,3 +159,5 @@ public class AuthService {
     }
 
 }
+
+
