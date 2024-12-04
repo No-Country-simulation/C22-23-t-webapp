4b65414409e513febb55ed8df6ce24a3f4be3238
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-12-04 09:24:06 -0300

    add comment for future reference

--- backend/src/main/java/com/SegundasHuellas/backend/auth/api/dto/AuthRegistrationRequest.java
+++ backend/src/main/java/com/SegundasHuellas/backend/auth/api/dto/AuthRegistrationRequest.java
@@ -3,6 +3,7 @@ package com.SegundasHuellas.backend.auth.api.dto;
 import com.SegundasHuellas.backend.auth.api.enums.UserRole;
 
 public record AuthRegistrationRequest(
+
         String email,
         String password,
         UserRole role
@@ -11,3 +12,27 @@ public record AuthRegistrationRequest(
         email = email != null ? email.toLowerCase().trim() : null;
     }
 }
+
+
+//public record PetProviderRegistrationRequest(
+//
+//        @Email
+//        @NotNull
+//        String email,
+//
+//        @StrongPassword
+//        String password,
+//
+//        @NotNull
+//        String passwordConfirmation
+//
+//) {
+//    public AuthRegistrationRequest {
+//        email = email != null ? email.toLowerCase().trim() : null;
+//    }
+//
+//    @AssertTrue(message = "Passwords do not match")
+//    private boolean passwordsMatch() {
+//        return password != null && password.equals(passwordConfirmation);
+//    }
+//}
