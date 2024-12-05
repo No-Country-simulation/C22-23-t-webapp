52214f54e2e5f51745d17fba9c2c0ab1515e4af9
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-12-05 12:48:15 -0300

    Add all remaining info to show on each pet on SearchPage

--- frontend/src/components/Pet/Pet.jsx
+++ frontend/src/components/Pet/Pet.jsx
@@ -1,8 +1,7 @@
 import './Pet.css'
 import { NavLink } from 'react-router-dom'
 
-
-export function Pet({ image, name, age, location}) {
+export function Pet({ image, name, age, species, gender, location, status}) {
     const convertAgeInDays = (age_in_days) => {
         if (age_in_days < 28)
             return `${age_in_days} día${age_in_days === 1 ? '' : 's'}`
@@ -17,14 +16,17 @@ export function Pet({ image, name, age, location}) {
     }
 
     return (
-    <li className='card-container'>
-        <img className='image-pet' src={ `./pet-${image}.jpeg` } alt={ `Foto de la mascota ${image}` } />
-        <div className='text-container'>
-            <h2 className='title-pet'>{ name }</h2>
-            <h3 className='age-pet'>{ convertAgeInDays(age) }</h3>
-            <h3 className='location-pet'>{ location }</h3>
-        </div>
-        <NavLink to={"/pet"} className='btn-pet'>Ver más detalles</NavLink>
-    </li>
+        <li className='card-container'>
+            <img className='image-pet' src={ `./pet-${image}.jpeg` } alt={ `Foto de la mascota ${image}` } />
+            <div className='text-container'>
+                <h2 className='title-pet'>{ name }</h2>
+                <h3 className='age-pet'>{ convertAgeInDays(age) }</h3>
+                <h3 className='species-pet'>{ species }</h3>
+                <h3 className='gender-pet'>{ gender }</h3>
+                <h3 className='location-pet'>{ location }</h3>
+                <h3 className='status-pet'>{ status }</h3>
+            </div>
+            <NavLink to={"/pet"} className='btn-pet'>Ver más detalles</NavLink>
+        </li>
     )
 }
--- frontend/src/components/PetList/PetList.jsx
+++ frontend/src/components/PetList/PetList.jsx
@@ -4,13 +4,16 @@ import { Pet } from '../'
 export function PetList({ pets }) {
     return (
         <ul>
-            {pets.map((pet, index)  =>  <Pet 
-                                            key={index}
-                                            image={pet.image}
-                                            name={pet.name}
-                                            age={pet.age}
-                                            location={pet.location}
-                                        />)}
+            {pets.map((pet)  => <Pet 
+                                    key={pet.id}
+                                    image={pet.id}
+                                    name={pet.name}
+                                    age={pet.ageInDays}
+                                    species={pet.species}
+                                    gender={pet.gender}
+                                    location={pet.location}
+                                    status={pet.status}
+                                />)}
         </ul>
     )
 }
