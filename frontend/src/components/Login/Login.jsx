b2998cb83eb2af3a55f458fbd5058b73ec6a3e7e
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-12-05 17:52:51 -0300

    Remove react-icons via NPM uninstall
    Remove imports that were no longer used

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./Login.css";
 import { Link, useNavigate } from "react-router-dom";
-import { FaGoogle, FaFacebook } from "react-icons/fa";
 
 const Login = () => {
   const [email, setEmail] = useState("");
