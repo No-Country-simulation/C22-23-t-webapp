25aad6e57301a2d51faa05dd4545b533354fb4b6
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-12-05 17:22:19 -0300

    Fix CSS styling issues that appeared after using info from backend
    Remove border from .PetInfoContainer to make design look more similar to wireframes

--- frontend/src/components/PetDetails/PetDetails.jsx
+++ frontend/src/components/PetDetails/PetDetails.jsx
@@ -17,37 +17,39 @@ export function PetDetails({ pet }) {
             
             <section id="PetDetailsInfoContainer" className="PetInfoContainer">
                 <h2 id="PetDetailsInfo" className="PetDetailsContainerLabel">Información de la mascota</h2>
-                
-                <p id="PetDetailsNameLabel" className="PetDetailsLabel"><strong>Nombre</strong></p>
-                <p id="PetDetailsName" className="PetDetailsText">{ pet.name }</p>
+                <div className="PetInfoList">
+                    <p id="PetDetailsNameLabel" className="PetDetailsLabel"><strong>Nombre</strong></p>
+                    <p id="PetDetailsName" className="PetDetailsText">{ pet.name }</p>
 
-                <p id="PetDetailsAgeLabel" className="PetDetailsLabel"><strong>Edad</strong></p>
-                <p id="PetDetailsAge" className="PetDetailsText">{ `${pet.ageInDays} días` }</p>
+                    <p id="PetDetailsAgeLabel" className="PetDetailsLabel"><strong>Edad</strong></p>
+                    <p id="PetDetailsAge" className="PetDetailsText">{ `${pet.ageInDays} días` }</p>
 
-                <p id="PetDetailsSpeciesLabel" className="PetDetailsLabel"><strong>Especie</strong></p>
-                <p id="PetDetailsSpecies" className="PetDetailsText">{ pet.species }</p>
+                    <p id="PetDetailsSpeciesLabel" className="PetDetailsLabel"><strong>Especie</strong></p>
+                    <p id="PetDetailsSpecies" className="PetDetailsText">{ pet.species }</p>
 
-                <p id="PetDetailsBreedLabel" className="PetDetailsLabel"><strong>Raza</strong></p>
-                <p id="PetDetailsBreed" className="PetDetailsText">{ pet.breedName }</p>
+                    <p id="PetDetailsBreedLabel" className="PetDetailsLabel"><strong>Raza</strong></p>
+                    <p id="PetDetailsBreed" className="PetDetailsText">{ pet.breedName }</p>
 
-                <p id="PetDetailsSizeLabel" className="PetDetailsLabel"><strong>Género</strong></p>
-                <p id="PetDetailsSize" className="PetDetailsText">{ pet.gender }</p>
+                    <p id="PetDetailsSizeLabel" className="PetDetailsLabel"><strong>Género</strong></p>
+                    <p id="PetDetailsSize" className="PetDetailsText">{ pet.gender }</p>
+                </div>
             </section>
 
             <section id="PetDetailsHealthStatusContainer" className="PetInfoContainer">
                 <h2 id="PetDetailsHealthStatus" className="PetDetailsContainerLabel">Salud de la mascota</h2>
+                <div className="PetInfoList">
+                    <p id="PetDetailsHealthStatusVaccinatedLabel" className="PetDetailsLabel"><strong>Vacunado</strong></p>
+                    <p id="PetDetailsHealthStatusVaccinated" className="PetDetailsText">{ pet.healthStatus.vaccinated ? 'Sí' : 'No' }</p>
 
-                <p id="PetDetailsHealthStatusVaccinatedLabel" className="PetDetailsLabel"><strong>Vacunado</strong></p>
-                <p id="PetDetailsHealthStatusVaccinated" className="PetDetailsText">{ pet.healthStatus.vaccinated ? 'Sí' : 'No' }</p>
+                    <p id="PetDetailsHealthStatusSterilizedLabel" className="PetDetailsLabel"><strong>Castrado</strong></p>
+                    <p id="PetDetailsHealthStatusSterilized" className="PetDetailsText">{ pet.isCastrated ? 'Sí' : 'No' }</p>
 
-                <p id="PetDetailsHealthStatusSterilizedLabel" className="PetDetailsLabel"><strong>Castrado</strong></p>
-                <p id="PetDetailsHealthStatusSterilized" className="PetDetailsText">{ pet.isCastrated ? 'Sí' : 'No' }</p>
+                    <p id="PetDetailsHealthStatusDewormedLabel" className="PetDetailsLabel"><strong>Desparacitado</strong></p>
+                    <p id="PetDetailsHealthStatusDewormed" className="PetDetailsText">{ pet.healthStatus.dewormed ? 'Sí' : 'No' }</p>
 
-                <p id="PetDetailsHealthStatusDewormedLabel" className="PetDetailsLabel"><strong>Desparacitado</strong></p>
-                <p id="PetDetailsHealthStatusDewormed" className="PetDetailsText">{ pet.healthStatus.dewormed ? 'Sí' : 'No' }</p>
-
-                <p id="PetDetailsHealthStatusMedicalConditionsLabel" className="PetDetailsLabel"><strong>Condiciones médicas</strong></p>
-                <p id="PetDetailsHealthStatusMedicalConditions" className="PetDetailsText">{ pet.healthStatus }</p>
+                    <p id="PetDetailsHealthStatusMedicalConditionsLabel" className="PetDetailsLabel"><strong>Condiciones médicas</strong></p>
+                    <p id="PetDetailsHealthStatusMedicalConditions" className="PetDetailsText">{ pet.healthStatus }</p>
+                </div>
             </section>
 
             <section id="PetDetailsExtraInfoContainer" className="PetInfoContainer">
@@ -59,15 +61,16 @@ export function PetDetails({ pet }) {
             
             <section id="PetDetailsContactContainer" className="PetInfoContainer">
                 <h2 id="PetDetailsContact" className="PetDetailsContainerLabel">Información de contacto</h2>
+                <div className="PetInfoList">
+                    <p id="PetDetailsLocationLabel" className="PetDetailsLabel"><strong>Ubicación</strong></p>
+                    <p id="PetDetailsLocation" className="PetDetailsText"> { pet.contactInfo.fullAddress } </p>
 
-                <p id="PetDetailsLocationLabel" className="PetDetailsLabel"><strong>Ubicación</strong></p>
-                <p id="PetDetailsLocation" className="PetDetailsText"> { pet.contactInfo.fullAddress } </p>
+                    <p id="PetDetailsRefugeePhoneLabel" className="PetDetailsLabel"><strong>Teléfono</strong></p>
+                    <p id="PetDetailsRefugeePhone" className="PetDetailsText"> { pet.contactInfo.phone } </p>
 
-                <p id="PetDetailsRefugeePhoneLabel" className="PetDetailsLabel"><strong>Teléfono</strong></p>
-                <p id="PetDetailsRefugeePhone" className="PetDetailsText"> { pet.contactInfo.phone } </p>
-
-                <p id="PetDetailsRefugeeEmailLabel" className="PetDetailsLabel"><strong>Email</strong></p>
-                <p id="PetDetailsRefugeeEmail" className="PetDetailsText"><Link to={ `mailto:${pet.contactInfo.email}` }> { pet.contactInfo.email } </Link></p>
+                    <p id="PetDetailsRefugeeEmailLabel" className="PetDetailsLabel"><strong>Email</strong></p>
+                    <p id="PetDetailsRefugeeEmail" className="PetDetailsText"><Link to={ `mailto:${pet.contactInfo.email}` }> { pet.contactInfo.email } </Link></p>
+                </div>
             </section>
         </>
     )
