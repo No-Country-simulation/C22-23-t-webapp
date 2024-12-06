2d60e9c26e91a7bf901aaae3e74cae7fe80f4a73
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-12-06 18:14:13 -0300

    Add endpoint to retrieve adopter details by userId in AdopterAuthController

--- backend/src/main/java/com/SegundasHuellas/backend/adopters/internal/infra/web/AdopterAuthController.java
+++ backend/src/main/java/com/SegundasHuellas/backend/adopters/internal/infra/web/AdopterAuthController.java
@@ -1,14 +1,12 @@
 package com.SegundasHuellas.backend.adopters.internal.infra.web;
 
+import com.SegundasHuellas.backend.adopters.internal.application.dto.AdopterDetailsResponse;
 import com.SegundasHuellas.backend.adopters.internal.application.dto.AdopterRegistrationRequest;
 import com.SegundasHuellas.backend.adopters.internal.application.service.AdopterRegistrationService;
 import com.SegundasHuellas.backend.auth.api.dto.AuthenticationResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth/adopters") // Agregamos "auth" para que no sea bloqueado por la config de seguridad
@@ -21,6 +19,11 @@ public class AdopterAuthController {
     @PostMapping("/register")
     public AuthenticationResponse register(@RequestBody @Valid AdopterRegistrationRequest request) {
         return registrationService.register(request);
+    }
+
+    @GetMapping("/{userId}")
+    public AdopterDetailsResponse getAdopterDetails(@PathVariable(name = "userId") Long userId) {
+        return registrationService.getAdopterDetails(userId);
     }
 
 }
