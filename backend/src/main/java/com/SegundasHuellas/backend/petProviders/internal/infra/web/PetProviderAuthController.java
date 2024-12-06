5f438a74ac27f62b6bbb17421d5395ed7b74bf8e
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2024-12-06 15:37:32 -0300

    Create and implement PetProviderAuthController.

--- backend/src/main/java/com/SegundasHuellas/backend/petProviders/internal/infra/web/PetProviderAuthController.java
+++ backend/src/main/java/com/SegundasHuellas/backend/petProviders/internal/infra/web/PetProviderAuthController.java
@@ -1,8 +1,11 @@
 package com.SegundasHuellas.backend.petProviders.internal.infra.web;
 
 
+import com.SegundasHuellas.backend.auth.api.dto.AuthenticationResponse;
+import com.SegundasHuellas.backend.petProviders.internal.application.dto.PetProviderRegistrationRequest;
 import com.SegundasHuellas.backend.petProviders.internal.application.service.PetProviderRegistrationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class PetProviderAuthController {
 
     private final PetProviderRegistrationService registrationService;
+
+    @PostMapping("/register")
+    public AuthenticationResponse registerPetProvider(PetProviderRegistrationRequest request) {
+        return registrationService.registerPetProvider(request);
+    }
 }
