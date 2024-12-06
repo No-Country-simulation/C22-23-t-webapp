90e968e312d8e1718ac862e4a46478f292152634
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-12-06 18:15:39 -0300

    Add query method in AdopterRepository to fetch adopter details by userId

--- backend/src/main/java/com/SegundasHuellas/backend/adopters/internal/infra/persistence/AdopterRepository.java
+++ backend/src/main/java/com/SegundasHuellas/backend/adopters/internal/infra/persistence/AdopterRepository.java
@@ -1,7 +1,27 @@
 package com.SegundasHuellas.backend.adopters.internal.infra.persistence;
 
+import com.SegundasHuellas.backend.adopters.internal.application.dto.AdopterDetailsResponse;
 import com.SegundasHuellas.backend.adopters.internal.domain.entity.Adopter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AdopterRepository extends JpaRepository<Adopter, Long> {
+
+
+    @Query("""
+            select new com.SegundasHuellas.backend.adopters.internal.application.dto.AdopterDetailsResponse(
+            a.userId,
+            a.firstName,
+            a.lastName,
+            a.status
+            )
+            from Adopter a
+            where a.userId = :userId
+            """)
+    Optional<AdopterDetailsResponse> findByUserId(@Param("userId") Long userId);
+
+
 }
