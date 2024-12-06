fec9b29c2e809993a90132826bb6f9039fc4709e
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-12-06 18:15:24 -0300

    Enhance AdopterRegistrationService to include userId in registration results and add method to retrieve adopter details by userId

--- backend/src/main/java/com/SegundasHuellas/backend/adopters/internal/application/service/AdopterRegistrationService.java
+++ backend/src/main/java/com/SegundasHuellas/backend/adopters/internal/application/service/AdopterRegistrationService.java
@@ -1,5 +1,6 @@
 package com.SegundasHuellas.backend.adopters.internal.application.service;
 
+import com.SegundasHuellas.backend.adopters.internal.application.dto.AdopterDetailsResponse;
 import com.SegundasHuellas.backend.adopters.internal.application.dto.AdopterRegistrationRequest;
 import com.SegundasHuellas.backend.adopters.internal.domain.entity.Adopter;
 import com.SegundasHuellas.backend.adopters.internal.domain.enums.AdopterStatus;
@@ -7,9 +8,12 @@ import com.SegundasHuellas.backend.adopters.internal.infra.persistence.AdopterRe
 import com.SegundasHuellas.backend.auth.api.RegistrationService;
 import com.SegundasHuellas.backend.auth.api.dto.AuthenticationResponse;
 import com.SegundasHuellas.backend.shared.domain.vo.Address;
+import com.SegundasHuellas.backend.shared.exception.DomainException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.SegundasHuellas.backend.shared.exception.DomainException.ErrorCode.RESOURCE_NOT_FOUND;
 
 @Service
 @Transactional
@@ -30,8 +34,16 @@ public class AdopterRegistrationService {
 
         adopter = adopterRepository.save(adopter);
 
-        return registrationService.register(request.toAuthRequest(), adopter.getId());
+        var registrationResults = registrationService.register(request.toAuthRequest(), adopter.getId());
+        adopter.setUserId(registrationResults.userId());
 
+        return registrationResults;
+
+    }
+
+    public AdopterDetailsResponse getAdopterDetails(Long userId) {
+        return adopterRepository.findByUserId(userId)
+                                .orElseThrow(() -> new DomainException(RESOURCE_NOT_FOUND, userId.toString()));
     }
 
 }
