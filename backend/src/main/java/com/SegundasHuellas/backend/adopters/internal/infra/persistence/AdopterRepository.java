234528e1e6d43b0f968384fd49a3f446d58bbbc9
Cristobal Pulgar E. <cristobal.npe@gmail.com>
2024-12-06 21:13:33 -0300

    Add summary retrieval method in AdopterRepository

--- backend/src/main/java/com/SegundasHuellas/backend/adopters/internal/infra/persistence/AdopterRepository.java
+++ backend/src/main/java/com/SegundasHuellas/backend/adopters/internal/infra/persistence/AdopterRepository.java
@@ -1,7 +1,10 @@
 package com.SegundasHuellas.backend.adopters.internal.infra.persistence;
 
 import com.SegundasHuellas.backend.adopters.internal.application.dto.AdopterDetailsResponse;
+import com.SegundasHuellas.backend.adopters.internal.application.dto.AdopterSummaryResponse;
 import com.SegundasHuellas.backend.adopters.internal.domain.entity.Adopter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -24,4 +27,14 @@ public interface AdopterRepository extends JpaRepository<Adopter, Long> {
     Optional<AdopterDetailsResponse> findByUserId(@Param("userId") Long userId);
 
 
+    @Query("""
+            select new com.SegundasHuellas.backend.adopters.internal.application.dto.AdopterSummaryResponse(
+                a.id,
+                a.firstName,
+                a.lastName,
+                a.status
+            )
+            from Adopter a
+            """)
+    Page<AdopterSummaryResponse> findAllSummaries(Pageable pageable);
 }
