a8e58c3adc310eb972a104a626b10d7a2afb45e1
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-12-07 21:02:34 -0300

    Add HeaderUserMenu to continue Header's refactor

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,24 +1,48 @@
 import "./Header.css"
 import { BASENAME } from '../../config.js'
 import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
 export function Header() {
+    const [ username, setUsername ] = useState()
+    
     return (
         <header id="Header">
             <nav id="HeaderNavbar">
+
                 <Link to={"/"} id="HeaderLogo">
+
                     <img id="HeaderLogoImage" src={`${BASENAME}/vite.svg`} alt="Logo de Vite.js" />
+
                     <h1 id="HeaderLogoTitle">SegundasHuellas</h1>
+
                 </Link>
-                <div id="HeaderSearchContainer">
-                    <img id="HeaderSearchIcon" src={`${BASENAME}/search-logo.svg`} alt="" />
-                    <input id="HeaderSearchInput" type="text" placeholder="Buscar perros, gatos y más..." />
-                </div>
+
+                <form id="HeaderSearchContainer">
+
+                    <input id="HeaderSearchInput" type="text" placeholder="Estoy buscando..." />
+
+                    <button id="HeaderSearchSubmit" type="submit">
+                        <img id="HeaderSearchIcon" src={`${BASENAME}/search-logo.svg`} alt="" />
+                    </button>
+
+                </form>
+
+                <button id="HeaderMenuButton">
+                    <img id="HeaderMenuButtonIcon" src={`${BASENAME}/sh-icon-menu.svg`} alt="" />
+                </button>
             </nav>
-            <div id="HeaderUserMenu">
-                <NavLink to={"/login"} id="HeaderLoginButton" className="HeaderUserMenuLink">Iniciar Sesión</NavLink>
-                <NavLink to={"/register"} id="HeaderRegisterButton" className="HeaderUserMenuLink">Únete</NavLink>
-            </div>
+            <ul id="HeaderUserMenu">
+                <li className="HeaderUserMenuItem">
+                    <h2>{`${username ? username : "Tu nuevo amigo te espera..."}`}</h2>
+                </li>
+                <li className="HeaderUserMenuItem">
+                    <NavLink to={"/login"} id="HeaderLoginButton" className="HeaderUserMenuLink">Iniciar Sesión</NavLink>
+                </li>
+                <li className="HeaderUserMenuItem">
+                    <NavLink to={"/register"} id="HeaderRegisterButton" className="HeaderUserMenuLink">Únete</NavLink>
+                </li>
+            </ul>
         </header>
     )
 }
