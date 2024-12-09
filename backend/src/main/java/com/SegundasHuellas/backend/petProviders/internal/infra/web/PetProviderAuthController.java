1ea96dd9225fa1e1fbc9fc40256a58184f314189
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2024-12-08 23:37:14 -0300

    Add getAllPetProvider and getPetProviderDetails endpoints in PetProviderController.

--- backend/src/main/java/com/SegundasHuellas/backend/petProviders/internal/infra/web/PetProviderAuthController.java
+++ backend/src/main/java/com/SegundasHuellas/backend/petProviders/internal/infra/web/PetProviderAuthController.java
@@ -2,15 +2,20 @@ package com.SegundasHuellas.backend.petProviders.internal.infra.web;
 
 
 import com.SegundasHuellas.backend.auth.api.dto.AuthenticationResponse;
+import com.SegundasHuellas.backend.petProviders.internal.application.dto.PetProviderDetailResponse;
 import com.SegundasHuellas.backend.petProviders.internal.application.dto.PetProviderRegistrationRequest;
+import com.SegundasHuellas.backend.petProviders.internal.application.dto.PetProviderSummaryResponse;
 import com.SegundasHuellas.backend.petProviders.internal.application.service.PetProviderService;
+import com.SegundasHuellas.backend.shared.application.dto.PageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("auth/petProvider")
+@RequestMapping("api/auth/pet-provider")
 @RequiredArgsConstructor
 public class PetProviderAuthController {
 
@@ -20,4 +25,15 @@ public class PetProviderAuthController {
     public AuthenticationResponse registerPetProvider(PetProviderRegistrationRequest request) {
         return registrationService.registerPetProvider(request);
     }
+    @GetMapping
+    public PageResponse<PetProviderSummaryResponse> getAllPetProviders(Pageable pageable) {
+        return registrationService.getAllPetProviders(pageable);
+    }
+
+    @GetMapping("/{userId}")
+    public PetProviderDetailResponse getPetProviderDetails(Long userId) {
+        return registrationService.getPetProviderDetails(userId);
+    }
+
+
 }
