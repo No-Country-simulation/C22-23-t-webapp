15bfbedcd24edd31ee8e135b630d9a1e510051b1
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-12-09 02:50:31 -0300

    Add primitive search filtering funcionality

--- frontend/src/components/PetListContainer/PetListContainer.jsx
+++ frontend/src/components/PetListContainer/PetListContainer.jsx
@@ -9,9 +9,33 @@ export function PetListContainer() {
     const GREETING = "Espera mientras preparamos el listado de mascotas..."
     const ERROR_MESSAGE = "¡UPS! Parece que no hay mascotas para adoptar ahora..."
 
-    const fetchPets = async () => {
+    const handleSearchSubmit = (searchFilters) => { fetchPets(searchFilters) }
+
+    const fetchPets = async (searchParams = {}) => {
         try {
-            const petsResponse = await fetch("http://localhost:8080/api/pets/search")
+            let url = "http://localhost:8080/api/pets/search"
+            
+            if (Object.keys(searchParams).length > 0) {
+                const filteredParams = []
+                
+                for (const key in searchParams) {
+                    if (searchParams.hasOwnProperty(key)) {
+                        const value = searchParams[key]
+                        
+                        // Solo parámetros con valor no vacío
+                        if (value !== "" && value !== null && value !== undefined) {
+                            filteredParams.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+                        }
+                    }
+                }
+                
+                if (filteredParams.length > 0) {
+                    url += `?${filteredParams.join("&")}`
+                    console.log("URL a fetchear", url)
+                }
+            }
+
+            const petsResponse = await fetch(url)
 
             const petsData = await petsResponse.json()
 
@@ -26,7 +50,7 @@ export function PetListContainer() {
 
     return (
         <main id="PetListContainer">
-            <PetSearchForm />
+            <PetSearchForm onSearch={ handleSearchSubmit } />
             {loading && <h2 id="SearchLoadingGreet">{GREETING}</h2>}
             {!loading && pets && <PetList pets={pets} />}
             {!loading && !pets && <h2 id="SearchErrorMessage">{ERROR_MESSAGE}</h2>}
--- frontend/src/components/PetSearchForm/PetSearchForm.jsx
+++ frontend/src/components/PetSearchForm/PetSearchForm.jsx
@@ -1,45 +1,122 @@
 import './PetSearchForm.css'
 import { BASENAME } from '../../config.js'
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
-export function PetSearchForm() {
+export function PetSearchForm({ onSearch }) {
     const [ isFilterMenuOpen, setIsFilterMenuOpen ] = useState(false)
+    const [searchFilters, setSearchFilters] = useState({
+        name: "",
+        species: "",
+        breed: "",
+        gender: "",
+        age: ""
+    })
+
+    const isFirstRender = useRef(true)
+    const previousSearchFilters = useRef(searchFilters)
+
+    const handleInputChange  = (event) => {
+        const { name, value } = event.target
+
+        setSearchFilters((previousFilters) => ({
+            ...previousFilters,
+            [name]: value,
+        }))
+    }
+    
+    const handleNameSearchSubmit  = (event) => {
+        event.preventDefault()
+        onSearch(searchFilters)
+    }
 
     const handleFilterMenuClick = () => { setIsFilterMenuOpen(!isFilterMenuOpen) }
 
+    useEffect(() => {
+        if (isFirstRender.current) {
+            isFirstRender.current = false
+            return
+        }
+        
+        // Comprobar si los filtros realmente cambiaron antes de llamar a onSearch
+        if (JSON.stringify(previousSearchFilters.current) !== JSON.stringify(searchFilters)) {
+            previousSearchFilters.current = searchFilters
+            onSearch(searchFilters)
+        }
+    }, [searchFilters, onSearch])
+
     return (
-        <form id="PetSearchForm">
+        <form id="PetSearchForm" onSubmit={ handleNameSearchSubmit }>
             <div id="PetSearchBarContainer">
                 <button id="PetSearchFilterButton" type="button" onClick={ handleFilterMenuClick }>
-                    <img id="PetSearchFilterButtonIcon" src={`${BASENAME}/sh-icon-${isFilterMenuOpen ? 'close' : 'filter-list'}.svg`} alt={`Icono ${isFilterMenuOpen ? 'cerrar' : 'abrir'} menú de filtros`} />
+                    <img
+                        id="PetSearchFilterButtonIcon"
+                        src={`${BASENAME}/sh-icon-${isFilterMenuOpen ? 'close' : 'filter-list'}.svg`}
+                        alt={`Icono ${isFilterMenuOpen ? 'cerrar' : 'abrir'} menú de filtros`}
+                    />
                 </button>
                 <div id="PetSearchByName">
-                    <input id="PetSearchByNameInput" type="text" placeholder="Buscar por nombre..." />
+                    <input id="PetSearchByNameInput" type="text" placeholder="Buscar por nombre..." name="name" value={ searchFilters.name } onChange={ handleInputChange }/>
                     <button id="PetSearchByNameSubmit" type="submit">
-                        <img id="PetSearchByNameIcon" src={`${BASENAME}/search-logo.svg`} alt="Icono botón buscar por nombre" />
+                        <img
+                            id="PetSearchByNameIcon"
+                            src={`${BASENAME}/search-logo.svg`}
+                            alt="Icono botón buscar por nombre"
+                        />
                     </button>
                 </div>
             </div>
             <div id="PetSearchFilterMenuContainer" className={ isFilterMenuOpen ? 'open' : '' }>
                 <h3 id="PetSearchFilterMenuLabel">Filtrar por:</h3>
-                <select name="" id="PetSearchFilterSpecies" className="PetSearchFilter">
-                    <option value="" className="PetSearchFilterOption" disabled selected>Especie</option>
-                    <option value="" className="PetSearchFilterOption">Perro</option>
-                    <option value="" className="PetSearchFilterOption">Gato</option>
-                    <option value="" className="PetSearchFilterOption">Otros</option>
+
+                {/* Filtro por Especie */}
+                <select
+                    name="species"
+                    id="PetSearchFilterSpecies"
+                    className="PetSearchFilter"
+                    onChange={ handleInputChange }
+                    value={ searchFilters.species }
+                >
+                    <option value="" className="PetSearchFilterOption" disabled>Especie</option>
+                    <option value="DOG" className="PetSearchFilterOption">Perro</option>
+                    <option value="CAT" className="PetSearchFilterOption">Gato</option>
+                    <option value="OTHER" className="PetSearchFilterOption">Otros</option>
                 </select>
-                <select name="" id="PetSearchFilterBreed" className="PetSearchFilter">
-                    <option value="" className="PetSearchFilterOption" disabled selected>Raza</option>
+
+                {/* Filtro por Raza */}
+                <select
+                    name="breed"
+                    id="PetSearchFilterBreed"
+                    className="PetSearchFilter"
+                    onChange={ handleInputChange }
+                    value={ searchFilters.breed }
+                >
+                    <option value="" className="PetSearchFilterOption" disabled>Raza</option>
                     <option value="" className="PetSearchFilterOption">Raza1</option>
                 </select>
-                <select name="" id="PetSearchFilterSex" className="PetSearchFilter">
-                    <option value="" className="PetSearchFilterOption" disabled selected>Sexo</option>
+
+                {/* Filtro por Sexo */}
+                <select
+                    name="gender"
+                    id="PetSearchFilterSex"
+                    className="PetSearchFilter"
+                    onChange={ handleInputChange }
+                    value={ searchFilters.gender }
+                >
+                    <option value="" className="PetSearchFilterOption" disabled>Sexo</option>
                     <option value="" className="PetSearchFilterOption">Hembra</option>
                     <option value="" className="PetSearchFilterOption">Macho</option>
                     <option value="" className="PetSearchFilterOption">No definido</option>
                 </select>
-                <select name="" id="PetSearchFilterAge" className="PetSearchFilter">
-                    <option value="" className="PetSearchFilterOption" disabled selected>Edad</option>
+
+                {/* Filtro por Edad */}
+                <select
+                    name="age"
+                    id="PetSearchFilterAge"
+                    className="PetSearchFilter"
+                    onChange={ handleInputChange }
+                    value={ searchFilters.age }
+                >
+                    <option value="" className="PetSearchFilterOption" disabled>Edad</option>
                     <option value="" className="PetSearchFilterOption">Menos de 1 año</option>
                     <option value="" className="PetSearchFilterOption">1 a 5 años</option>
                     <option value="" className="PetSearchFilterOption">5 a 10 años</option>
