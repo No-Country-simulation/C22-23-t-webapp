fd81b18fcb283c713b36796c9e1844af951f387c
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2024-12-10 18:42:35 -0300

    add comments about adoptions requests.

--- backend/src/main/java/com/SegundasHuellas/backend/adoptions/package-info.java
+++ backend/src/main/java/com/SegundasHuellas/backend/adoptions/package-info.java
@@ -1,4 +1,7 @@
 @ApplicationModule(displayName = "Adoptions")
 package com.SegundasHuellas.backend.adoptions;
+/*
+    correo electronico, id de adopter, id de pet, id de provider, id de adopcion, token loogin para quien es, edad, direccion, telefono.
+ */
 
 import org.springframework.modulith.ApplicationModule;
