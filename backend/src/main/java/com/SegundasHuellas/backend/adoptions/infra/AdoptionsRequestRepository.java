41e4da4733d58e4db32f0939accf1593ca803dc2
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2024-12-20 17:44:22 -0300

    modify the adoptionRequests repository to add custom methods

--- backend/src/main/java/com/SegundasHuellas/backend/adoptions/infra/AdoptionsRequestRepository.java
+++ backend/src/main/java/com/SegundasHuellas/backend/adoptions/infra/AdoptionsRequestRepository.java
@@ -4,7 +4,14 @@ import com.SegundasHuellas.backend.adoptions.domain.entity.AdoptionRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AdoptionsRequestRepository extends JpaRepository<AdoptionRequest, Long> {
 
+    List<AdoptionRequest> findAllByAdopterId(Long adopterId);
+
+    List<AdoptionRequest> findAllByPetId(Long petId);
+
+    List<AdoptionRequest> findAllByPetProviderId(Long petProviderId);
 }
